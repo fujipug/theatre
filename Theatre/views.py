3416562c06675aa8bbfe340c7948f6275ba7d84d
Ian Johnson <itj3@nau.edu>
2015-02-02 15:19:16 -0700

    basic template example
    Display home.html as a template. Just showing how to set it up in
    views.py

--- Theatre/views.py
+++ Theatre/views.py
@@ -1,5 +1,13 @@
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    #dictionary to be passed into the tepmlate render object.
+    random_dictionary = {'testing': "this is a template test..."}
+    #gets template from Template folder
+    template = get_template('home.html')
+    #fills in template tags with corresponing text from dicitonary
+    html = template.render(Context(random_dictionary))
+    return HttpResponse(html)
